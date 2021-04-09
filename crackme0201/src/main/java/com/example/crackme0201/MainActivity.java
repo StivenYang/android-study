f271aa0f53153c0b34c64a57653f5858fc3dd929
StivenYang <StivenYang626@gmail.com>
2021-04-09 19:18:36 +0800

    提交线性布局

--- crackme0201/src/main/java/com/example/crackme0201/MainActivity.java
+++ crackme0201/src/main/java/com/example/crackme0201/MainActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +18,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(R.string.unregister);
+        EditText username = findViewById(R.id.username);
+        EditText password = findViewById(R.id.password);
+        Button btn_register = findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(v -> {
+            if (!checkSN(username.getText().toString().trim(), password.getText().toString().trim())) {
+                Toast.makeText(MainActivity.this, R.string.unsuccessed, Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(MainActivity.this,
+                        R.string.successed, Toast.LENGTH_SHORT).show();
+                btn_register.setEnabled(false);
+                setTitle(R.string.register);
+            }
+        });
     }
 
     private boolean checkSN(String username, String sn){
@@ -28,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
             digest.reset();
             digest.update(username.getBytes());
             byte[] bytes = digest.digest();
-            String hexstr = (bytes, "");
+            String hexstr = Arrays.toString(bytes);
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < hexstr.length(); i += 2) {
                 sb.append(hexstr.charAt(i));
